869bcf75b348c861456a98a69359aa51d8de78ab
nithinbadicodes <nbadi1350@gmail.com>
2025-07-21 12:29:49 +0530

    Keys pressed turned into one function

--- Game2/main.py
+++ Game2/main.py
@@ -7,12 +7,11 @@ pygame.display.set_caption('Shooter game')
 
 
 
-
-BG_IMAGE = pygame.image.load('Game2/assets/space.png')
+BG_IMAGE = pygame.image.load('assets/space.png')
 BG = pygame.transform.scale(BG_IMAGE,(WIDTH,HEIGHT))
 
-YELLOW_SPACESHIP_IMAGE = pygame.transform.rotate(pygame.image.load('Game2/assets/spaceship_yellow.png'),90)
-RED_SPACESHIP_IMAGE = pygame.transform.rotate(pygame.image.load('Game2/assets/spaceship_red.png'),-90)
+YELLOW_SPACESHIP_IMAGE = pygame.transform.rotate(pygame.image.load('assets/spaceship_yellow.png'),90)
+RED_SPACESHIP_IMAGE = pygame.transform.rotate(pygame.image.load('assets/spaceship_red.png'),-90)
 
 SPACESHIP_SIZE = 35
 SPACESHIP_VELOCITY = 6
@@ -28,6 +27,39 @@ def draw(yellow,red):
     WIN.blit(player1,(yellow.x,yellow.y))
     WIN.blit(player2,(red.x,red.y))
     pygame.display.update()
+
+
+
+
+
+def get_key_pressed(keys,yellow,red,player:str):
+    if player == "yellow":
+        #Yellow spaceship movement and boundries
+        if keys[pygame.K_w] and yellow.y >=0:
+            yellow.y-=SPACESHIP_VELOCITY
+        if keys[pygame.K_s] and yellow.y <= HEIGHT - SPACESHIP_SIZE:
+            yellow.y+=SPACESHIP_VELOCITY
+        if keys[pygame.K_a] and yellow.x>=0:
+            yellow.x-=SPACESHIP_VELOCITY
+        if keys[pygame.K_d] and yellow.x <= WIDTH/2 -SPACESHIP_SIZE:
+            yellow.x+=SPACESHIP_VELOCITY
+    
+    elif player == "red":
+        #Red spaceship movement and boundries
+        if keys[pygame.K_UP] and red.y >=0:
+            red.y -= SPACESHIP_VELOCITY
+        if keys[pygame.K_DOWN] and red.y <= HEIGHT - SPACESHIP_SIZE:
+            red.y += SPACESHIP_VELOCITY
+        if keys[pygame.K_LEFT] and red.x >= WIDTH/2:
+            red.x -= SPACESHIP_VELOCITY
+        if keys[pygame.K_RIGHT] and red.x <= WIDTH - SPACESHIP_SIZE:
+            red.x += SPACESHIP_VELOCITY
+
+
+
+
+
+
 
 
 def main():
@@ -54,26 +86,29 @@ def main():
 
         keys = pygame.key.get_pressed()
 
-        #Yellow spaceship movement and boundries
-        if keys[pygame.K_w] and yellow.y >=0:
-            yellow.y-=SPACESHIP_VELOCITY
-        if keys[pygame.K_s] and yellow.y <= HEIGHT - SPACESHIP_SIZE:
-            yellow.y+=SPACESHIP_VELOCITY
-        if keys[pygame.K_a] and yellow.x>=0:
-            yellow.x-=SPACESHIP_VELOCITY
-        if keys[pygame.K_d] and yellow.x <= WIDTH/2 -SPACESHIP_SIZE:
-            yellow.x+=SPACESHIP_VELOCITY
+        get_key_pressed(keys,yellow,red,"yellow")
+        get_key_pressed(keys,yellow,red,"red")
+
+        # #Yellow spaceship movement and boundries
+        # if keys[pygame.K_w] and yellow.y >=0:
+        #     yellow.y-=SPACESHIP_VELOCITY
+        # if keys[pygame.K_s] and yellow.y <= HEIGHT - SPACESHIP_SIZE:
+        #     yellow.y+=SPACESHIP_VELOCITY
+        # if keys[pygame.K_a] and yellow.x>=0:
+        #     yellow.x-=SPACESHIP_VELOCITY
+        # if keys[pygame.K_d] and yellow.x <= WIDTH/2 -SPACESHIP_SIZE:
+        #     yellow.x+=SPACESHIP_VELOCITY
 
 
-        #Red spaceship movement and boundries
-        if keys[pygame.K_UP] and red.y >=0:
-            red.y -= SPACESHIP_VELOCITY
-        if keys[pygame.K_DOWN] and red.y <= HEIGHT - SPACESHIP_SIZE:
-            red.y += SPACESHIP_VELOCITY
-        if keys[pygame.K_LEFT] and red.x >= WIDTH/2:
-            red.x -= SPACESHIP_VELOCITY
-        if keys[pygame.K_RIGHT] and red.x <= WIDTH - SPACESHIP_SIZE:
-            red.x += SPACESHIP_VELOCITY
+        # #Red spaceship movement and boundries
+        # if keys[pygame.K_UP] and red.y >=0:
+        #     red.y -= SPACESHIP_VELOCITY
+        # if keys[pygame.K_DOWN] and red.y <= HEIGHT - SPACESHIP_SIZE:
+        #     red.y += SPACESHIP_VELOCITY
+        # if keys[pygame.K_LEFT] and red.x >= WIDTH/2:
+        #     red.x -= SPACESHIP_VELOCITY
+        # if keys[pygame.K_RIGHT] and red.x <= WIDTH - SPACESHIP_SIZE:
+        #     red.x += SPACESHIP_VELOCITY
         
 
 
